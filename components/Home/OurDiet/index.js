d32c8b1b0df2de973bca18dbbe58d77e2252d5a3
Julio Zeledón <julio@Julios-MacBook-Pro.local>
2022-02-17 14:16:41 -0600

    added our diet section

--- components/Home/OurDiet/index.js
+++ components/Home/OurDiet/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 import ResponsiveImage from '../../ResponsiveImage'
 import banner from '../../../public/images/titular_scrll_1-01.svg'
+import meat from '../../../public/images/our-diet-meat.svg'
+import mix from '../../../public/images/our-diet-mix.svg'
+import balance from '../../../public/images/our-diet-balance.svg'
 import styles from './OurDiet.module.scss'
 
 const OurDiet = () => {
@@ -14,12 +17,18 @@ const OurDiet = () => {
             </h2>
             <div className={styles.columns}>
                 <div className={styles.column}>
+                    <div className={styles.headerImage}>
+                        <ResponsiveImage src={meat} alt='dibujo de carne' />
+                    </div>
                     <p>
                         Utilizamos una dieta acorde a las necesidades de los <span>carnívoros facultativos y carnívoros 
                         estrictos</span>.
                     </p>
                 </div>
                 <div className={styles.column}>
+                    <div className={styles.headerImage}>
+                        <ResponsiveImage src={mix} alt='ingredientes siendo mezclados' />
+                    </div>
                     <p>
                         Todos nuestros ingredientes cumplen con un propósito y función por lo que no nos adherimos a
                         dietas pre estipuladas, sino que nos enfocamos en <span>promover el equilibrio holístico que
@@ -27,6 +36,9 @@ const OurDiet = () => {
                     </p>
                 </div>
                 <div className={styles.column}>
+                    <div className={styles.headerImage}>
+                        <ResponsiveImage src={balance} alt='una dieta equilibrada' />
+                    </div>
                     <p>
                         Libre de almidones, preservantes; cruda, balanceada y provee los nutrientes esenciales para un
                         correcto mantenimiento de su salud.
