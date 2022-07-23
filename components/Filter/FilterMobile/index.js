ba373ab39440b3d012c3a9ecfe0ff21e9b494f6c
Julio Zeledón <julio@Julios-MacBook-Pro.local>
2022-07-23 17:28:15 -0600

    split filters for mobile and desktop

---
+++ components/Filter/FilterMobile/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
+
+// local imports
+// styles
+import styles from './FilterMobile.module.scss'
+
+const FilterMobile = ({ optionsList, selectedOption }) => {
+    const [showList, setShowList] = useState(false)
+    const [activeOption, setActiveOption] = useState(selectedOption.id)
+
+    return (
+        <div className={styles.filter}>
+            <div
+                className={styles.filterHeader}
+                role='button'
+                tabIndex='0'
+                onClick={() => setShowList(!showList)}
+            >
+                <span>Catálogo&nbsp;</span>
+                <span
+                    style={{
+                        transform: `rotate(${showList ? '180' : '0'}deg)`,
+                    }}
+                    className={styles.icon}
+                >
+                    <FontAwesomeIcon icon={faCaretDown} />
+                </span>
+            </div>
+            <ul className={`${styles.filterList} ${showList ? styles.show : ''}`}>
+            {optionsList.map(item => {
+                return (<li
+                    role='button'
+                    key={`filter-by-${item.id}`}
+                    tabIndex='0'
+                    onClick={() => {
+                        item.onClick(item.id)
+                        setActiveOption(item.id)
+                    }}
+                >
+                    <span style={{
+                        textDecoration: activeOption === item.id ? 'underline' : 'none'
+                    }}
+                    >
+                        {item.label}
+                    </span>
+                </li>)
+            })}
+            </ul>
+        </div>
+    )
+}
+ 
+export default FilterMobile
