6f60c97ee7d1f59f2cbd432c10d8421170a8c51f
Julio Zeledón <julio@Julios-MacBook-Pro.local>
2023-01-20 14:07:35 -0600

    fixes

--- pages_components/Home/index.js
+++ pages_components/Home/index.js
@@ -1,31 +1,40 @@
-import React from 'react'
+import React from 'react';
 
 // local imports
 // styles
-import styles from './Home.module.scss'
+import styles from './Home.module.scss';
 
 // components
-import Hero from './Hero'
-import Welcome from './Welcome'
-import Benefits from './Benefits'
-import Products from './Products'
-import Contact from './Contact'
-import DNAtureSystem from './DNAtureSystem'
-import OurCostumers from './OurCostumers'
-
+import Hero from './Hero';
+import Welcome from './Welcome';
+import Benefits from './Benefits';
+import Products from './Products';
+import Contact from './Contact';
+import DNAtureSystem from './DNAtureSystem';
+import OurCostumers from './OurCostumers';
 
 const Home = () => {
-    return (
-        <div className={styles.home}>
-            <Hero />
-            <Welcome />
-            <Benefits />
-            <Products />
-            <DNAtureSystem />
-            <OurCostumers />
-            <Contact />
-        </div>
-    )
-}
- 
-export default Home
+  function callApi() {
+    fetch('https://dnature-be.herokuapp.com/details', { method: 'GET' })
+      .then((data) => data.json())
+      .then((json) => console.log(JSON.stringify(json)));
+  }
+
+  useEffect(() => {
+    callApi();
+  }, []);
+
+  return (
+    <div className={styles.home}>
+      <Hero />
+      <Welcome />
+      <Benefits />
+      <Products />
+      <DNAtureSystem />
+      <OurCostumers />
+      <Contact />
+    </div>
+  );
+};
+
+export default Home;
