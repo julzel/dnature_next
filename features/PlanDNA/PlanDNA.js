9dcaf2abe2df46d176dea0203aaa41c27264ecf8
julzel <jusher22@gmail.com>
2023-03-22 08:01:15 -0600

    styles

---
+++ features/PlanDNA/PlanDNA.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PlanDNA = () => {
+  return (
+    <div>PlanDNA component</div>
+  );
+};
+
+export default PlanDNA;
---
+++ features/PlanDNA/PlanDNAContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PlanDNA from './PlanDNA';
+
+const PlanDNAContainer = () => {
+  return (
+    <PlanDNA />
+  );
+};
+
+export default PlanDNAContainer;
