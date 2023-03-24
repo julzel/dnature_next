0588958603381d26d4349e146bd4b5439a31c4bb
julzel <jusher22@gmail.com>
2023-03-24 06:51:33 -0600

    more styles

--- features/Home/DNAtureSystem/index.js
+++ features/Home/DNAtureSystem/index.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 // local imports
 // styles
@@ -11,10 +10,11 @@ import DNAturePlanSteps from "./dnature-plan-steps";
 
 // components
 import Button from "../../../components/Button";
+import ModalContainer from "../../../components/Modal";
 
 const PlanSteps = () => {
-  const router = useRouter();
-  
+  const [displayModal, setDisplayModal] = useState(false);
+
   return (
     <>
       <section className={styles.dnatureSystem}>
@@ -35,7 +35,7 @@ const PlanSteps = () => {
               className={styles.button}
               onClick={(e) => {
                 e.preventDefault();
-                router.push("/plan-dnature");
+                setDisplayModal(true);
               }}
             >
               Conocer más
@@ -44,16 +44,16 @@ const PlanSteps = () => {
         </div>
         <div className={styles.dnaturePlate}>
           <div className={styles.content}>
-          <h3>¡Transforma la vida de tu mascota!</h3>
-          <ul>
-            <li>Proteína cruda de alta calidad</li>
-            <li>Nutrientes esenciales para una vida sana</li>
-            <li>Menús personalizados para cada peludo amigo</li>
-            <li>Servicio a domicilio: ¡comodidad y frescura garantizada!</li>
-          </ul>
+            <h3>¡Transforma la vida de tu mascota!</h3>
+            <ul>
+              <li>Proteína cruda de alta calidad</li>
+              <li>Nutrientes esenciales para una vida sana</li>
+              <li>Menús personalizados para cada peludo amigo</li>
+              <li>Servicio a domicilio: ¡comodidad y frescura garantizada!</li>
+            </ul>
 
-          <h4>¡Regala salud y felicidad a tu mejor amigo!</h4>
-        </div>
+            <h4>¡Regala salud y felicidad a tu mejor amigo!</h4>
+          </div>
         </div>
         <div className={styles.benefits}>
           <h2 className={`title ${styles.title}`}>Beneficios</h2>
@@ -68,6 +68,37 @@ const PlanSteps = () => {
             ))}
           </div>
         </div>
+        {displayModal && (
+          <ModalContainer closeModal={() => setDisplayModal(false)}>
+            <div className={styles.planInfo}>
+              <h2>¡Gracias por tu interés!</h2>
+              <p>
+                Queremos informarte que nuestro servicio de PLAN DNA únicame
+                está disponible a través de nuestro WhatsApp, por el momento.
+              </p>
+
+              <p>
+                Trabajamos arduamente para implementar esta función en nuestro
+                sitio web lo antes posible para que puedas acceder a este y
+                otros servicios de forma aún más conveniente y fácil.
+              </p>
+
+              <p>
+                Por favor, no dude en contactarnos a través de WhatsApp si tiene
+                alguna pregunta o necesita ayuda. ¡Estamos aquí para ayudarte
+                con todo lo relacionado a la nutrición de tus mascotas!
+              </p>
+
+              <div>
+                <a
+                  href={`https://api.whatsapp.com/send?phone=50671732328&text=Deseo información sobre el plan de alimentación DNAture.`}
+                >
+                  <span>Contáctanos</span>
+                </a>
+              </div>
+            </div>
+          </ModalContainer>
+        )}
       </section>
     </>
   );
