3023d35cf893b00d54e5833389f20fae75d2218d
julzel <jusher22@gmail.com>
2023-03-29 12:05:31 -0600

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,33 @@
+import React, { useEffect } from "react";
+import ReactGA from "react-ga";
+import { useRouter } from "next/router";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+
 // local imports
 // styles
 import "../styles/globals.scss";
+
+// providers
 import ShoppingCartContextProvider from "../contexts/shopping-cart-context";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  const router = useRouter();
+
+  // Initialize Google Analytics tracking code
+  useEffect(() => {
+    ReactGA.initialize(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID);
+  }, []);
+
+  // Track pageviews on route change
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ReactGA.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <ShoppingCartContextProvider>
       <Component {...pageProps} />
