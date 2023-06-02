dd213eb775917c91494118a5f3e372d67d49a920
julzel <jusher22@gmail.com>
2023-06-02 17:36:16 -0600

    product navigates to previous url

--- features/Product/GoBack/index.js
+++ features/Product/GoBack/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleLeft } from '@fortawesome/free-regular-svg-icons';
 
-const GoBack = ({ className }) => (
-  <div className={className}>
-    <Link href={'/productos'} passHref>
+const GoBack = ({ className }) => {
+  const router = useRouter();
+
+  const goBack = (event) => {
+    event.preventDefault();
+    router.back();
+  }
+
+  return (
+    <div className={className} onClick={goBack}>
       <span>
         <FontAwesomeIcon icon={faCircleLeft} size='1x' />
         &nbsp; Volver
       </span>
-    </Link>
-  </div>
-);
+    </div>
+  );
+};
 
 export default GoBack;
