abbbf27ddf76ba2d605358f47f84b1395853a01b
julzel <jusher22@gmail.com>
2023-11-12 17:40:37 -0600

    completed boxes for nutrition plans

--- features/Home/Appointments/index.js
+++ features/Home/Appointments/index.js
@@ -1,64 +1,49 @@
 import React from 'react';
-import Image from 'next/image';
-import {
-  Button,
-  Card,
-  CardContent,
-  CardMedia,
-  Grid,
-  Typography,
-  Box,
-  CardActions,
-} from '@mui/material';
-
-// // local imports
-import nutri1 from '../../../public/images/nutri/nutri_1.jpeg';
-import nutri2 from '../../../public/images/nutri/nutri_2.jpeg';
-import logoBlack from '../../../public/images/dnature-logo.svg';
+import { Button, Typography, Box, Divider } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShieldDog } from '@fortawesome/free-solid-svg-icons';
 import styles from './Appointments.module.scss';
 import CursiveTitle from '../../../components/CursiveTitle';
 
-// Componente ServiceCard para mostrar información sobre cada tipo de consulta
-const ServiceCard = ({ title, description, price, image }) => (
-  <Card>
-    <CardMedia>
-      <Box
-      sx={{
-        overflow: 'hidden',
-        height: {
-          xs: '250px', // extra-small devices - screen width: 0px or larger
-          sm: '300px', // small devices - screen width: 600px or larger
-          md: '350px', // medium devices - screen width: 900px or larger
-          // add more breakpoints as needed
-        },
-      }}>
-        <Image
-          src={image}
-          alt="DNAture Logo"
-        />
-      </Box>
-    </CardMedia>
+// Constants for styles
+const MARGIN_TOP = '2rem';
+const MARGIN_BOTTOM = '2rem';
+const TEXT_ALIGN_CENTER = 'center';
 
-    <CardContent>
-      <Typography variant="h6" component="div" gutterBottom>
+// AppointmentTypeBox Subcomponent
+const AppointmentTypeBox = ({ title, description, price }) => (
+  <Box className={styles['appointments_type']} p={2} m={1}>
+    <Box className={styles['appointments_type-title']}>
+      <FontAwesomeIcon icon={faShieldDog} size="lg" />
+      <Typography
+        variant="h6"
+        component="h6"
+        gutterBottom
+        align={TEXT_ALIGN_CENTER}
+        textTransform="uppercase"
+        mb={2}
+      >
         {title}
       </Typography>
-      <Typography variant="body2" color="text.secondary">
+    </Box>
+    <Divider />
+    <Box>
+      <Typography variant="body2" mt={2}>
         {description}
       </Typography>
-      <Typography variant="h5" component="div" sx={{ marginTop: '10px' }}>
+      <Typography
+        variant="h4"
+        component="h4"
+        align="right"
+        sx={{ marginTop: '20px' }}
+      >
         {price}
       </Typography>
-      <CardActions sx={{ justifyContent: 'flex-end' }}>
-        <Button color="primary" size="small">
-          Leer más
-        </Button>
-      </CardActions>
-    </CardContent>
-  </Card>
+    </Box>
+  </Box>
 );
 
-// Componente principal NutritionSection
+// NutritionSection Component
 const NutritionSection = () => {
   return (
     <Box className={styles['appointments-container']}>
@@ -69,9 +54,9 @@ const NutritionSection = () => {
           variant="body1"
           color="text"
           sx={{
-            marginTop: '2rem',
-            marginBottom: '2rem',
-            textAlign: 'center',
+            marginTop: MARGIN_TOP,
+            marginBottom: MARGIN_BOTTOM,
+            textAlign: TEXT_ALIGN_CENTER,
           }}
         >
           La salud de tu mascota comienza con su nutrición. Ofrecemos consultas
@@ -79,31 +64,24 @@ const NutritionSection = () => {
           bienestar a través de la alimentación. Descubre nuestros planes:
         </Typography>
 
-        <Grid container spacing={8} justifyContent="center">
-          <Grid item xs={12} sm={6}>
-            <ServiceCard
-              title="Bases de la Nutrición Saludable"
-              description="Ideal para propietarios que buscan entender y mejorar la dieta diaria de sus mascotas. Incluye evaluación inicial y recomendaciones personalizadas."
-              price="Costo $60"
-              image={nutri1}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <ServiceCard
-              title="Plan para Necesidades Específicas"
-              description="Diseñado para mascotas con condiciones particulares, este plan incluye una estrategia de alimentación detallada, seguimiento y ajustes sin costo adicional."
-              price="Costo $90"
-              image={nutri2}
-            />
-          </Grid>
-        </Grid>
-        <Box
-          mt={6}
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-          }}
-        >
+        <Box className={styles['appointments_types']}>
+          <AppointmentTypeBox
+            title="Plan para necesidades integrales"
+            description="Ideal para propietarios que buscan entender y mejorar la dieta
+            diaria de sus mascotas. Incluye evaluación inicial y
+            recomendaciones personalizadas."
+            price="$60"
+          />
+          <AppointmentTypeBox
+            title="Plan para necesidades específicas"
+            description="Diseñado para mascotas con condiciones particulares, este plan
+            incluye una estrategia de alimentación detallada, seguimiento y
+            ajustes sin costo adicional."
+            price="$90"
+          />
+        </Box>
+
+        <Box mt={6} sx={{ display: 'flex', justifyContent: 'center' }}>
           <Button variant="outlined" color="primary" size="large">
             Agenda tu Consulta Hoy
           </Button>
