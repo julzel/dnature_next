e174018e8e5c73ce662ce52bec8d3bf377877c07
Julio Zeledón <julio@MacBook-Pro-Julio.local>
2024-04-30 15:52:21 -0600

    implements image carousel for product info

--- features/Product/ProductInfo/ProductInfo.js
+++ features/Product/ProductInfo/ProductInfo.js
@@ -1,14 +1,31 @@
 import Link from 'next/link';
 import { Box } from '@mui/material';
 import Image from 'next/image';
+import Slider from 'react-slick';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import PresentationSelector from '../../../components/PresentationSelector'; // Components
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 // local imports
 // styles
 import styles from './ProductInfo.module.scss';
 import QuickAdd from '../../../components/QuickAdd';
+
+// components
+import PresentationSelector from '../../../components/PresentationSelector'; // Components
+
+const sliderSettings = {
+  dots: true, // Shows dot indicators at the bottom
+  infinite: true, // Infinite looping
+  speed: 500, // Animation speed
+  slidesToShow: 1, // Number of slides to show at a time
+  slidesToScroll: 1, // Number of slides to scroll
+  initialSlide: 0, // Initial slide index
+  swipeToSlide: true, // Allows slides to be dragged
+  adaptiveHeight: true, // Adjusts height to the current slide
+  arrows: true, // Enables navigation arrows
+};
 
 const ProductInfo = ({
   productDetail,
@@ -23,17 +40,20 @@ const ProductInfo = ({
   <>
     <div className={styles.productInfo}>
       <div className={styles.images}>
-        {productDetail.images?.map((img, i) => (
-          <Image
-            key={i}
-            src={img.url}
-            alt={img.title}
-            width='100%'
-            height='100%'
-            layout='responsive'
-            objectFit='contain'
-          />
-        ))}
+        <Slider {...sliderSettings}>
+          {productDetail.images?.map((img, i) => (
+            <div key={i}>
+              <Image
+                src={img.url}
+                alt={img.title}
+                width='100%'
+                height='100%'
+                layout='responsive'
+                objectFit='contain'
+              />
+            </div>
+          ))}
+        </Slider>
       </div>
       <div className={styles.info}>
         <div>
@@ -42,7 +62,10 @@ const ProductInfo = ({
             <p className={styles.price}>
               ₡{selectedPresentation ? selectedPresentation.price : ''}{' '}
               {selectedPresentation && (
-                <span className={styles.small}> | {selectedPresentation.size}</span>
+                <span className={styles.small}>
+                  {' '}
+                  | {selectedPresentation.size}
+                </span>
               )}
             </p>
           ) : (
@@ -53,38 +76,35 @@ const ProductInfo = ({
           )}
         </div>
         {hasPriceByUnit && (
-            <Box my={4} width='100%'>
-              <PresentationSelector
-                presentations={productDetail.preciosPorUnidad}
-                selectedPresentation={selectedPresentation}
-                onPresentationSelect={handlePresentationSelect}
-              />
-            </Box>
-          )}
-        <Box
-          pt={2}
-          pb={4}
-          mb={2}
-          display='flex'
-          justifyContent='center'
-          sx={{ transform: 'scale(1.25)' }}
-        >
-          <QuickAdd
-            itemsInCart={itemsInCart}
-            removeOneItemFromCart={() => onRemoveOneItem(productDetail.sys.id)}
-            addItemToCart={onAddToCart}
-          />
-        </Box>
-
-        {cartTotalItems > 0 && (
-          <Link href={'/cart'} passHref>
-            <a>
-              Ver Carrito
-              <FontAwesomeIcon icon={faCartShopping} />
-              <span>({cartTotalItems})</span>
-            </a>
-          </Link>
+          <Box my={4} width='100%'>
+            <PresentationSelector
+              presentations={productDetail.preciosPorUnidad}
+              selectedPresentation={selectedPresentation}
+              onPresentationSelect={handlePresentationSelect}
+            />
+          </Box>
         )}
+        <Box display='flex' flexDirection='column' alignItems={['center', 'flex-end']}>
+          <Box pt={2} mb={4} sx={{ transform: 'scale(1.25)' }}>
+            <QuickAdd
+              itemsInCart={itemsInCart}
+              removeOneItemFromCart={() =>
+                onRemoveOneItem(productDetail.sys.id)
+              }
+              addItemToCart={onAddToCart}
+            />
+          </Box>
+
+          {cartTotalItems > 0 && (
+            <Link href={'/cart'} passHref>
+              <a>
+                Ver Carrito
+                <FontAwesomeIcon icon={faCartShopping} />
+                <span>({cartTotalItems})</span>
+              </a>
+            </Link>
+          )}
+        </Box>
       </div>
     </div>
   </>
