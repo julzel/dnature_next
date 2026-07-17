ceb0426e22a76e7c2531a3f91064611efd578512
julzel <jusher22@gmail.com>
2026-07-17 17:33:43 -0600

    setup: app router migration

--- features/Product/formatDescription.js
+++ features/Product/formatDescription.js
@@ -1,17 +1,12 @@
-const NEW_LINE = '<br />';
-const SPAN = '</span>';
-
 const formatProductDescription = (description) => {
   if (!description) {
     return '';
   }
 
   return description
-    .replaceAll('-', `${NEW_LINE}- `)
-    .replaceAll('_', '<span>')
-    .replaceAll('%', `%${SPAN}${NEW_LINE}`)
-    .replace('@', `${NEW_LINE}${NEW_LINE}<div>`)
-    .replace('@', '<div>');
+    .replaceAll('@', '\n\n')
+    .replaceAll('_', '')
+    .replaceAll('%', '');
 };
 
 export default formatProductDescription;
