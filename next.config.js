fc6d55358e80402c47dacfae292bb4735ed454fb
julzel <jusher22@gmail.com>
2026-07-18 20:12:40 -0600

    update depracated Saas import directive

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   distDir: process.env.E2E_DIST_DIR || '.next',
-  ...(process.env.E2E_USE_FIXTURES === '1'
-    ? {
-        sassOptions: {
-          silenceDeprecations: ['import', 'global-builtin', 'color-functions'],
-        },
-      }
-    : {}),
   reactStrictMode: true,
   trailingSlash: true,
   images: {
